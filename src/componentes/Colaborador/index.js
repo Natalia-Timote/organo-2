90d59efc4df6e4a6af00ae9caae06c326f0aaff8
Natalia Timote <nataliamirian.nt@gmail.com>
2025-01-28 19:05:25 -0300

    favoritar colaboradores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,18 @@
 import { IoIosCloseCircle } from "react-icons/io";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar,
+        color: "#FF0000"
+    }
+
     return (<div className="colaborador">
         <IoIosCloseCircle 
             size={24} 
@@ -14,6 +25,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito
+                ? <FaHeart {...propsFavorito} /> 
+                : <FaRegHeart {...propsFavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
